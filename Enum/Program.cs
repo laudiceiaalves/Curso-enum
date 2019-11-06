08a0646f1baadb8c5c3438c6042468ffe512f867
Laudiceia Alves <alves.laudiceiaa@icloud.com>
2019-11-06 15:46:45 -0300

    Correcao de erros e mudança de status

---
+++ Enum/Program.cs
@@ -0,0 +1,26 @@
+﻿using Enum.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Enum.Entities.Enums;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.Processing
+            };
+
+            Console.WriteLine(order);
+            }
+        }
+    }
+
